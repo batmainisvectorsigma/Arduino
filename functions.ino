c17b79545a0d4078f067b9b69c77b19095782e2e
whereyoucan'tfind <74447376+batmainisvectorsigma@users.noreply.github.com>
2023-10-14 18:32:00 +0545

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,4 +1,5 @@
-//Example No 1 : Blink Example
+//Example No 1 : Blink Example 
+//Blinking only one led
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);                                     
 }
@@ -10,4 +11,26 @@ void myBlinkFunction(){
   delay(1000);                       
   digitalWrite(LED_BUILTIN, LOW);               
   delay(1000);  
+}
+
+//Example No 2: Blink Example
+//Bliking multiple led
+int led1=9;
+int led2=7;
+int led3=2;
+void setup(){
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+}
+void loop(){
+  myCustomLED(led1);
+  myCustomLED(led2);
+  myCustomLED(led3);
+}
+void myCustomLED(int pin){
+  digitalWrite(pin,HIGH);
+  delay(1000);
+  digitalWrite(pin,LOW);
+  delay(1000);
 }
