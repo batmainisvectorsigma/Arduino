dd785670f64ec6d78fbe41cafd7b47599d19aa79
whereyoucan'tfind <74447376+batmainisvectorsigma@users.noreply.github.com>
2023-10-14 13:29:48 +0545

    Update control_structure.ino

--- control_structure.ino
+++ control_structure.ino
@@ -1,2 +1,13 @@
 void setup(){
   Serial.begin(9600);
+  if(true){
+    Serial.println("The program is runnning");
+  }
+  else{
+    Serial.println("The program is not runnning");
+  }
+}
+void loop(){
+  //here the program will run repeateadly
+}
+//There are lot more concepts but other concepts are lot more save as that we use in java
