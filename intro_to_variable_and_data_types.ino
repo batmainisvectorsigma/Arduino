551661f5899c5f37d5119fbe0b50ec1a3ea90c19
whereyoucan'tfind <74447376+batmainisvectorsigma@users.noreply.github.com>
2023-10-14 08:45:00 +0545

    Create intro_to_variable_and_data_types.ino

---
+++ intro_to_variable_and_data_types.ino
@@ -0,0 +1,17 @@
+// Topic 1 : Variables
+/*
+1: int 
+range -32768 to 32767
+can store positive and negative value
+declaration is similar to java 
+no initialization the defualt value is 0
+-> Some functions
+serial communication between arduino and computer 
+Have to write this function if you want any communication between arduino and computer
+Serial.begin(9600);
+whatever you want to print in serial monitor
+Serial.print();
+Serial.print(); adds one line when printing
+
+
+*/
